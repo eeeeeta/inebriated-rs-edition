b7e96a9ae942a3ec3f4801aff5009bd631cb6d66
eeeeeta <eeeeeta@users.noreply.github.com>
2016-02-12 18:03:18 +0000

    inebriated Rust edition: terrible start code

---
+++ README.md
@@ -0,0 +1,21 @@
+inebriated, Rust edition (uber-αlpha)
+====================================
+*everybody's favourite drunkard!*
+
+(no logo yet)
+
+wat?
+---
+
+The inebriated project line is a series of implementations of the same thing: a drunk AI that takes
+input sentences and uses them to form stuff that sort-of makes sense and is sometimes funny.
+
+
+This is the Rust implementation. Currently, it's *very very alpha* and not ready for doing anything yet.
+
+Links to the other inebriateds:
+
+- [the inebriated project spotlight](http://theta.eu.org/inebriated-family.html)
+- [inebriated: the original edition](https://github.com/eeeeeta/inebriated-genesis)
+- [inebriated, C edition](https://github.com/eeeeeta/inebriated-c-edition)
+- [inebriated, C++ edition](https://github.com/eeeeeta/inebriated-cpp-edition)
---
+++ src/main.rs
@@ -0,0 +1,45 @@
+extern crate inebriated;
+use std::io;
+use std::io::prelude::*;
+
+fn main() {
+    let mut db = inebriated::Markov::new();
+    println!("inebriated, Rust edition (αlpha)\nan eta thing <http://theta.eu.org>\n");
+    println!("input sentences, newline-separated (Ctrl-D to stop)");
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let mut key = Some("".to_string());
+        let mut val = None::<String>;
+        let mut spaces = 0;
+        for hunk in line.unwrap().split(" ") {
+            spaces += 1;
+            let closure = |k: &mut String| {
+                if k != "" {
+                    k.push_str(" ");
+                }
+                k.push_str(hunk);
+            };
+            if val.is_some() {
+                val.as_mut().map(closure);
+            }
+            else {
+                key.as_mut().map(closure);
+            }
+            if spaces == 2 {
+                if val.is_none() {
+                    db.add_starter(key.as_ref().unwrap().to_owned());
+                }
+                else {
+                    db.insert(key.take().unwrap(), val.as_ref().unwrap().to_owned());
+                    key = Some(val.take().unwrap());
+                }
+                val = Some("".to_string());
+                spaces = 0;
+            }
+        }
+        if val.as_ref().unwrap() != "" {
+            db.insert(key.take().unwrap(), val.take().unwrap());
+        }
+    };
+    db.generate_sentence();
+}
